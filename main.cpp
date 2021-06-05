a0baf0bd7839c53978cbf12144141662b6997759
창훈 박 <>
2021-06-05 15:19:29 +0000

    hello sdl 분리하기

--- main.cpp
+++ main.cpp
@@ -2,28 +2,61 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
+bool g_bRunning = false;
 
-int main(int argc, char* args[])
+bool init(const char* title, int xpos, int ypos, 
+          int height, int width, int flags)
 {
-  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
-    g_pWindow = SDL_CreateWindow("Chapter 1: Setting up SDL",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        300, 300, SDL_WINDOW_SHOWN);
+    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+    {
 
-    if (g_pWindow != 0) {
-        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        g_pWindow = SDL_CreateWindow( title, 
+                                      xpos, ypos, 
+                                      height, width, flags);
+
+        if (g_pWindow != 0)
+        {
+            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+        }
+
     }
-  }
-  else {
-      return 1;
-  }
+    else
+    {
+        return false;
+    }
 
-  SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
-  SDL_RenderClear(g_pRenderer);
- // SDL_RenderPresent(g_pRenderer);
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
-  SDL_Delay(5000);
-  SDL_Quit();
-  return 0;
+    return true;
+}
+
+void render()
+{
+    SDL_RenderClear(g_pRenderer);
+    SDL_RenderPresent(g_pRenderer);
+}
+
+int main(int argc, char* argv[])
+{
+    if (init("Breaking Up HelloSDL", 
+              SDL_WINDOWPOS_CENTERED,
+              SDL_WINDOWPOS_CENTERED, 
+              640, 480,
+              SDL_WINDOW_SHOWN))
+    {
+        g_bRunning = true;
+    }
+    else
+    {
+        return 1; // something's wrong
+    }
+
+    while (g_bRunning)
+    {
+        // handle input - update - render
+        render();
+    }
+
+    SDL_Quit();
+    return 0;
 }
